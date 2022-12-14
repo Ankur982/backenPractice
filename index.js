b0f6df55934f6457decfb66b01df6117c012b378
Ankur982 <singh999ankur@gmail.com>
2022-12-14 22:05:34 +0530

    adding

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const mongoose = require("mongoose");
+
+const authRoute = require("./routes/User");
+
+const cors = require("cors");
+
+const MONGO_URL = 'mongodb+srv://Ankur:ankur@cluster0.c4ohs0g.mongodb.net/rm201'
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.use("/user" , authRoute);
+
+
+
+
+mongoose.connect( MONGO_URL )
+.then(()=>{
+    console.log("DB connection successfull")
+})
+.catch((err)=>{
+    console.log(err)
+});
+
+app.listen( 8080, () => { 
+    console.log('server started on port 8080');
+});
